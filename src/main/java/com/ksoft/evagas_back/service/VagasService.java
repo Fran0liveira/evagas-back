bc63848e1b4c19ff2edb72de0347c4dee8d78f09
Francisco Leonardo Leite Oliveira <francisco.leonardo11@outlook.com>
2025-04-08 06:20:01 -0300

    criando branch dev

--- src/main/java/com/ksoft/evagas_back/service/VagasService.java
+++ src/main/java/com/ksoft/evagas_back/service/VagasService.java
@@ -18,6 +18,7 @@ public class VagasService {
 	
 	public List<VagaDto> getVagas()
 	{
+		System.out.println("CriANDO BRaNCH dev");
 		return vagasRepository.findAll()
 				.stream()
 				.map(v-> v.toDto()
